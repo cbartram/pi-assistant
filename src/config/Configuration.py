e99df534faaefcdf0fee9da8669b799b9e03e14a
Christian <cbartram3@gmail.com>
2022-03-30 11:32:12 -0400

    Added config logic and unit tests

--- src/config/Configuration.py
+++ src/config/Configuration.py
@@ -1,21 +1,60 @@
 import os
 import yaml
+from src.log import logger
+
 
 class Configuration:
+    """
+    Loads and provides an access point to a single source of application configuration
+    """
 
     def __init__(self, environment: str = os.getenv("ENVIRONMENT", "LOCAL")):
         self._environment = environment
         # Always load application.yml as the default configuration but if additional env specific
         # configuration is present it should overwrite values specified in application.yml
-        with open('resources/application.yml', 'r') as file:
-            self._yaml_config = yaml.safe_load(file)
-        print(self._yaml_config)
+        try:
+            with open(os.path.join('resources', 'application.yml'), 'r') as file:
+                self._yaml_config = yaml.safe_load(file)
+        except Exception as e:
+            logger.error(f"Exception thrown while attempting to load application.yaml configuration properties. Error = {str(e)}")
+            self._yaml_config = {}
+
+        # Attempt to load any environment specific configuration
+        path = os.path.join('resources', f'application-{environment.lower()}.yml')
+        if os.path.exists(path):
+            try:
+                with open(path, 'r') as file:
+                    # Merge two configuration files overwriting values from the environment specific config
+                    self._yaml_config.update(yaml.safe_load(file))
+            except Exception as e:
+                logger.error(f"Exception thrown while attempting to load {path} configuration properties. Error = {str(e)}")
+
+        else:
+            logger.debug(f"No environment specific configuration exists for the environment: {environment.lower()}")
 
     def get(self, key: str) -> str:
-        pass
+        if key.endswith("."):
+            raise KeyError("The key cannot end with a \".\"")
+
+        if self._yaml_config is None or self._yaml_config is {}:
+            raise Exception(f"Cannot retrieve configuration from null config. Using key: {key}")
+
+        parts = key.split(".")
+        data = None
+        for k in parts:
+            if data is None:
+                if k in self._yaml_config:
+                    data = self._yaml_config[k]
+                else:
+                    raise KeyError(f"The key part: {k} from the given key: {key} does not exist in the configuration.")
+            else:
+                if k in data:
+                    data = data[k]
+                else:
+                    raise KeyError(f"The key part: {k} from the given key: {key} does not exist in the configuration.")
+        return data
 
     def get_environment(self):
         return self._environment
-
 
 
--- test/config/test_configuration.py
+++ test/config/test_configuration.py
@@ -1,7 +1,54 @@
+import pytest
 from src.config import Configuration
 
 
 def test_configuration_load_application_default_values_correctly():
-    config = Configuration()
-    assert 1 == 1
+    config = Configuration(environment="prod")
+    assert config._yaml_config['voice_assistant'] is not None
+    assert len(config._yaml_config['voice_assistant']['keywords']) == 3
 
+
+def test_configuration_load_application_merges_values_correctly():
+    config = Configuration(environment="local")
+    assert config._yaml_config['voice_assistant'] is not None
+    assert len(config._yaml_config['voice_assistant']['keywords']) == 1
+
+
+def test_configuration_get_environment():
+    config = Configuration(environment="local")
+    assert config.get_environment() == "local"
+
+
+def test_configuration_get_key():
+    config = Configuration(environment="prod")
+    keywords = config.get("voice_assistant.keywords")
+    assert len(keywords) == 3
+    assert keywords[0]['text'] == 'hey google'
+
+
+def test_configuration_get_throws_error_on_invalid_key():
+    try:
+        config = Configuration(environment="prod")
+        config.get("voice_assistant.invalid_key.")
+    except Exception as e:
+        assert "The key cannot end with a" in str(e)
+
+
+def test_configuration_get_throws_error_on_failure_to_load_yaml():
+    try:
+        config = Configuration(environment="prod")
+        config._yaml_config = None
+        config.get("voice_assistant.keywords")
+    except Exception as e:
+        assert str(e) == "Cannot retrieve configuration from null config. Using key: voice_assistant.keywords"
+
+@pytest.mark.parametrize("test_input,expected", [("voice_assistant.non_existent",
+                                                  "The key part: non_existent from the given key: voice_assistant.non_existent does not exist in the configuration."),
+                                                 ("bad_initial_key.keywords",
+                                                  "The key part: bad_initial_key from the given key: bad_initial_key.keywords does not exist in the configuration.")])
+def test_configuration_get_throws_error_on_non_existent_key(test_input, expected):
+    try:
+        config = Configuration(environment="prod")
+        config.get(test_input)
+    except Exception as e:
+        assert "The key part:" in str(e)
